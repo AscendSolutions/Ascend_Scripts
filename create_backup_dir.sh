a3c7141c06206f0e25d565ecf691ef0c4a35e180
ramihelal <rami.talat@gmail.com>
2015-02-16 17:49:53 +0200

    Create create_backup_dir.sh

---
+++ create_backup_dir.sh
@@ -0,0 +1,18 @@
+############################################################
+# create_backup_dir.sh
+# Creates the backup_dir and postgres crontab
+# Created by Rami Talat 08-JAN-2014
+# 
+#
+############################################################
+# Location to place backups.
+backup_dir="/var/backups/postgres_db"
+sudo mkdir $backup_dir
+sudo chown postgres $backup_dir
+# create the backup script ...
+sudo su - postgres -c "wget https://raw.githubusercontent.com/ramitalat/Ascend_Solutions/master/Buraq/VM/backup.sh"
+sudo chmod +x backup.sh
+# modifying postgres crontab
+sudo su - postgres -c "crontab -l | { cat; echo \"45 */4 * * * /var/lib/postgresql/backup.sh > /dev/null 2>&1\"; } | crontab -"
+# return back to your current dir
+cd -
